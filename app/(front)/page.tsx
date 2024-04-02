c5206e3f6ac84d913ec5e2567dde767dca06a742
Ashminkhadka <ashminkhadka25@gmail.com>
2024-04-02 18:20:31 +0545

    commit navbar and searchbar

---
+++ app/(front)/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/frontend/Navbar";
+import Hero from "@/components/frontend/Hero";
+import React from "react";
+
+export default function Home(){
+  return(
+    <section className= "">
+      <Navbar/>
+      <Hero/>
+    </section>
+  )
+}
