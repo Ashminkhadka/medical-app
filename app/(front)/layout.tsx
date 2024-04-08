3e07d89f0f749cb2f3c4d8f7fb6902aa3e2656df
Ashminkhadka <ashminkhadka25@gmail.com>
2024-04-08 08:34:17 +0545

    Added Mega menu

--- app/(front)/layout.tsx
+++ app/(front)/layout.tsx
@@ -1,12 +1,19 @@
+import MegaMenu from '@/components/frontend/MegaMenu'
+import Navbar from '@/components/frontend/Navbar'
 import React, { ReactNode } from 'react'
 
 export default function Layout({children}: {children:ReactNode}) {
   return (
-    <div>
+    <div className="bg-blue-950">
+      <Navbar/>
+      <div className="max-w-5xl mx-auto py-6 flex justify-between">
+        <MegaMenu/>
+      </div>
       {children}
     </div>
   )
 }
+
 
 
 
--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,11 +1,11 @@
 import Navbar from "@/components/frontend/Navbar";
 import Hero from "@/components/frontend/Hero";
 import React from "react";
+import MegaMenu from "@/components/frontend/MegaMenu";
 
 export default function Home(){
   return(
     <section className= "">
-      <Navbar/>
       <Hero/>
     </section>
   )
